409ac2f3de488b52a4caafd6d17708a0d1405a51
Kyroro <kyroroluc@gmail.com>
2023-07-26 17:53:14 +0300

    sucses

--- python_2.py
+++ python_2.py
@@ -5,11 +5,24 @@
 
 user_int = int(input())
 k = 0
-for i in range(n):
+for i in range(user_int):
     v = int(input())
     if v == 1:
         k += 1
-print(k if k<n/2 else n-k)
+print(k if k < user_int / 2 else user_int - k)
 
 
+#Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. 
+#Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
+#Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
 
+a, b = map(int, input().split())
+c = 0
+for i in range(a + b):
+    if c:
+        break
+    for j in range(a + b):
+        if i + j == a and i * j == b:
+            c = 1
+            print(*sorted([i, j]))
+            break
